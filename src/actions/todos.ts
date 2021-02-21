7b4f792ac36962ba0e6b469c2ddf41e36c1a3977
Joshua Pare <65102852+sbhrule15@users.noreply.github.com>
2021-02-21 15:42:38 -0500

    refactoring actions to clean up imports

---
+++ src/actions/todos.ts
@@ -0,0 +1,41 @@
+import axios from 'axios';
+import { Dispatch } from 'redux';
+import { ActionTypes } from './types';
+
+// define return shape from API
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+// additional type safety
+export interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
+export interface DeleteTodoAction {
+  type: ActionTypes.deleteTodo;
+  payload: number;
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+export const fetchTodos = () => {
+  return async (dispatch: Dispatch) => {
+    const response = await axios.get<Todo[]>(url);
+
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.fetchTodos,
+      payload: response.data,
+    });
+  };
+};
+
+export const deleteTodo = (id: number): DeleteTodoAction => {
+  return {
+    type: ActionTypes.deleteTodo,
+    payload: id,
+  };
+};
